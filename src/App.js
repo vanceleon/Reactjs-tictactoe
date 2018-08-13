16a7c74e533de2a211ad9027b8cf59025506f985
vanceleon <vanceleon44@gmail.com>
2018-08-13 00:29:45 -0400

    squares added to the game

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Square from './components/Squares';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+
+class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
+
+
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({squares: squares});
+  }
+
+
+  renderSquare(i) {
+    return <Square value={i}/>;
+  }
+
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <h1>Tic-Tac-Toe game built in React!</h1>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+        </div>
+
+      </div>
+    )
+  }
+
+
+
 }
 
-export default App;
+export default Board;
